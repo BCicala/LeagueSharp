d845835d404f538c458ff4af397e1de3348c3d79
Prunes Assemblies <ikillbananas68@gmail.com>
2014-11-26 17:35:25 -0500

    Removed unused code

--- Master Yi by Prunes/Master Yi by Prunes/Program.cs
+++ Master Yi by Prunes/Master Yi by Prunes/Program.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LeagueSharp;
 using LeagueSharp.Common;
-using SharpDX;
 namespace MasterYiByPrunes
 {
     class Program
