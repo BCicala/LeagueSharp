331e126a78e0bbf56f44fe891c52207ceb6562d4
Prunes Assemblies <ikillbananas68@gmail.com>
2014-11-26 16:47:03 -0500

    Uploaded Master yi

---
+++ Master Yi by Prunes/Master Yi by Prunes/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+namespace MasterYiByPrunes
+{
+    class Program
+    {
+        public const string ChampName = "MasterYi";
+        public static Orbwalking.Orbwalker Orbwalker;
+        public static Obj_AI_Base Player = ObjectManager.Player;
+        public static Spell Q, W, E, R;
+        private static Items.Item tiamatItem, hydraItem, botrkItem, bilgeItem, randuinsItem, GhostbladeItem;
+        public static Menu Config;
+        public static List<Spell> SpellList = new List<Spell>();
+
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            if (Player.BaseSkinName != ChampName) return;
+
+            Q = new Spell(SpellSlot.Q, 600);
+            Q.SetTargetted(0.5f, 2000);
+            W = new Spell(SpellSlot.W);
+            E = new Spell(SpellSlot.E);
+            R = new Spell(SpellSlot.R);
+
+            SpellList.Add(Q);
+            SpellList.Add(W);
+            SpellList.Add(E);
+            SpellList.Add(R);
+
+            bilgeItem= new Items.Item(3144, 475f);
+            botrkItem = new Items.Item(3153, 425f);
+            hydraItem = new Items.Item(3074, 250f);
+            tiamatItem = new Items.Item(3077, 250f);
+            randuinsItem = new Items.Item(3143, 490f);
+            GhostbladeItem = new Items.Item(3142, 590f);
+
+
+            Config = new Menu("Prunes" + ChampName, ChampName, true);
+            var ts = new Menu("Target Selector", "Target Selector");
+            SimpleTs.AddToMenu(ts);
+            Config.AddSubMenu(ts);
+            Config.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
+            Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
+
+            Config.AddSubMenu(new Menu("Combo", "Combo"));
+            Config.SubMenu("Combo").AddItem(new MenuItem("useQ", "Use Q?").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("useW", "Use W?").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("useE", "Use E?").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("useR", "Use R?").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("ComboActive", "Combo").SetValue(new KeyBind(32, KeyBindType.Press)));
+            Config.AddToMainMenu();
+
+            Game.OnGameUpdate += Game_OnGameUpdate;
+            Game.PrintChat("Master Yi by Prunes");
+        }
+
+        static void Game_OnGameUpdate(EventArgs args)
+        {
+            if (Config.Item("ComboActive").GetValue<KeyBind>().Active)
+            {
+                Combo();
+            }
+        }
+
+
+        public static void Combo()
+        {
+            var target = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Physical);
+            if (target == null) return;
+
+            var useQ = Config.Item("UseQ").GetValue<bool>();
+            var useW = Config.Item("UseW").GetValue<bool>();
+            var useE = Config.Item("UseE").GetValue<bool>();
+            var useR = Config.Item("UseE").GetValue<bool>();
+
+            if (target.IsValidTarget(Q.Range) && R.IsReady() && useR)
+            {
+                R.Cast();
+            }
+            if (target.IsValidTarget(Q.Range) && Q.IsReady() && useQ)
+            {
+                Q.CastOnUnit(target);
+            }
+            if (target.IsValidTarget(Q.Range) && E.IsReady() && useE)
+            {
+                E.Cast();
+            }
+            else if (target.IsValidTarget(Q.Range) && W.IsReady() && Orbwalking.InAutoAttackRange(target) && useW)
+            {
+                Player.IssueOrder(GameObjectOrder.AttackTo, target);
+                Utility.DelayAction.Add(350, () => W.Cast());
+                Player.IssueOrder(GameObjectOrder.AttackTo, target);
+                Orbwalking.ResetAutoAttackTimer();
+            }
+            if (tiamatItem.IsReady() && target.IsValidTarget(tiamatItem.Range))
+            {
+                tiamatItem.Cast();
+            }
+            if (hydraItem.IsReady() && target.IsValidTarget(tiamatItem.Range))
+            {
+                hydraItem.Cast();
+            }
+            if (botrkItem.IsReady() && target.IsValidTarget(botrkItem.Range))
+            {
+                botrkItem.Cast();
+            }
+            if (bilgeItem.IsReady() && target.IsValidTarget(bilgeItem.Range))
+            {
+                bilgeItem.Cast();
+            }
+            if (GhostbladeItem.IsReady() && target.IsValidTarget(Q.Range))
+            {
+                GhostbladeItem.Cast();
+            }
+            if (randuinsItem.IsReady() && target.IsValidTarget(randuinsItem.Range))
+            {
+                randuinsItem.Cast();
+            }
+        }
+    }
+}
